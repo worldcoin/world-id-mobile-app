3a3e2efe91c071e79c41ba07e0bda530fc35956f
Paolo D'Amico <paolodamico@users.noreply.github.com>
2022-12-08 20:51:34 +0100

    UI 1

--- screens/WorldIDScreen.tsx
+++ screens/WorldIDScreen.tsx
@@ -1,23 +1,58 @@
-import { StyleSheet } from "react-native";
+import { ImageBackground, StyleSheet } from "react-native";
 import Button from "../components/Button";
+import Tag from "../components/Tag";
 import { Text, View } from "../components/Themed";
+import { fontLg, h1Style, white } from "../styles";
 import { RootTabScreenProps } from "../types";
+const constellation = require("../assets/images/constellation.png");
+const profilePicPolygon = require("../assets/images/profile-pic-polygon.png");
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
+  },
+  heroContainer: {
+    paddingTop: 84,
+    width: "100%",
+    paddingBottom: 16,
+  },
+  titleLine: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  profilePictureWrapper: {
+    backgroundColor: "transparent",
+    alignItems: "center",
+    paddingTop: 16,
+  },
+  profilePictureContainer: {
+    height: 104,
+    width: 104,
+  },
+  profilePictureTextView: {
+    flex: 1,
+    alignItems: "center",
     justifyContent: "center",
+    width: "100%",
+    height: "100%",
+    backgroundColor: "transparent",
+  },
+  profilePicture: {
+    fontSize: 40,
   },
   title: {
-    paddingTop: 16,
-    fontSize: 20,
-    fontWeight: "bold",
+    ...h1Style,
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
+  textId: {
+    paddingTop: 4,
+    fontSize: fontLg,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  levelContainer: {
+    backgroundColor: "transparent",
+    paddingTop: 8,
   },
 });
 
@@ -26,13 +61,28 @@ export default function WorldIDScreen({
 }: RootTabScreenProps<"WorldID">) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Your World ID</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-      <Button label="Generate ZKP" onPress={() => console.log(1)} />
+      <ImageBackground
+        source={constellation}
+        style={styles.heroContainer}
+        resizeMode="cover"
+      >
+        <Text style={styles.title}>Your World ID</Text>
+        <View style={styles.profilePictureWrapper}>
+          <ImageBackground
+            source={profilePicPolygon}
+            style={styles.profilePictureContainer}
+            resizeMode="contain"
+          >
+            <View style={styles.profilePictureTextView}>
+              <Text style={styles.profilePicture}>👻</Text>
+            </View>
+          </ImageBackground>
+        </View>
+        <Text style={styles.textId}>#ALC2031</Text>
+        <View style={styles.levelContainer}>
+          <Tag label="LEVEL 1" />
+        </View>
+      </ImageBackground>
     </View>
   );
 }
---
+++ styles.tsx
@@ -0,0 +1,17 @@
+import { TextStyle } from "react-native";
+
+// Colors
+export const grayDark6 = "#191C20";
+export const white = "#FFFFFF";
+
+// Font sizes
+export const fontLg = 18;
+export const fontMd = 14;
+
+export const h1Style: TextStyle = {
+  fontSize: 30,
+  fontWeight: "bold",
+  color: white,
+  paddingLeft: 16,
+  paddingRight: 16,
+};
